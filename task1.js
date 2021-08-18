f31e3559eb628824f31381036118dd50fb8afaf0
Shahzada <md.shahzada1606@gmail.com>
2021-08-19 02:46:29 +0530

    Yeah website responsive work has been complete now. Add ZLife image foder with images

--- task1.js
+++ task1.js
@@ -22,3 +22,24 @@ function scrollFunction() {
     document.getElementById("navbar").style.backgroundColor = "transparent";
   }
 }
+
+// Script for Life in zippy section 
+
+let pictures = document.getElementsByClassName('pictures')
+
+for (var i = 0; i < pictures.length; i++) {
+  pictures[i].addEventListener('mouseover', function () {
+    document.getElementById('show-images').src = this.src
+  })
+}
+// ------ For Left Arrow ------ 
+let leftArrow = document.getElementById('leftArrow');
+leftArrow.addEventListener('click', function () {
+  document.getElementById('move').scrollLeft -= 80
+})
+
+// ------ For Right Arrow ------ 
+let rightArrow = document.getElementById('rightArrow');
+rightArrow.addEventListener('click', function () {
+  document.getElementById('move').scrollLeft += 80
+})
